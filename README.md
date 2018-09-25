897deeb3a1836c148a97957fdc395cdfc3437f95
Lukas Stateczny <statecznylukas@gmail.com>
2018-09-25 19:36:30 +0000

    refactored the microservice using apex for deployment to lambda succesfully provided the api

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # karma-points
-Microservice storing user's karma points
+Microservice storing and retrieving karma points of a specific user
+
+
+*GET FUNCTION*
+
+URL for API:
+https://u0mxny2nq6.execute-api.us-east-2.amazonaws.com/default/karma-points_get?id=USER_ID
+
+*Replace USER_ID at the end of the url with the four digit user id*
+
+Response returns json object with the id, name and karma-points of the user.
+
+I created a mock data in dynamoDB in AWS Lambda
+*Please use one of these mock id's to test the microservice: [1234, 2345, 3456, 4567, 5678]*
---
+++ functions/get/index.js
@@ -0,0 +1,29 @@
+// console.log('starting function')
+// exports.handle = function(e, ctx, cb) {
+//   console.log('processing event: %j', e)
+//   cb(null, { hello: 'world' })
+// }
+
+console.log('starting function')
+
+const AWS = require('aws-sdk')
+const docClient = new AWS.DynamoDB.DocumentClient({region: "us-east-2"})
+
+exports.handle = function(e, ctx, callback) {
+
+    let id = e.id
+    
+    let params = {
+        TableName: 'karma_points',
+        Key: {
+          "id": id
+        }
+    }
+    docClient.get(params, function(err, data) {
+        if (err) {
+            callback(err, null)
+        } else {
+            callback(null, data)
+        }
+    })
+}
