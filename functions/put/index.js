05bb23ec1dbe3dcfd8678d1055c2b9b3676e65cd
Lukas Stateczny <statecznylukas@gmail.com>
2018-09-29 21:25:30 +0000

    Update Item function handles new users and automaticaly inserts them into the db if they don't exist

--- functions/put/index.js
+++ functions/put/index.js
@@ -1,9 +1,3 @@
-// console.log('starting function')
-// exports.handle = function(e, ctx, cb) {
-//   console.log('processing event: %j', e)
-//   cb(null, { hello: 'world' })
-// }
-
 console.log('GET function')
 
 const AWS = require('aws-sdk')
@@ -12,6 +6,14 @@ const docClient = new AWS.DynamoDB.DocumentClient({region: "us-east-2"})
 exports.handle = function(e, ctx, callback) {
 
     let id = e.id
+
+    function isEmpty(obj) {
+        for(var key in obj) {
+            if(obj.hasOwnProperty(key))
+                return false;
+        }
+        return true;
+    }
     
     let params = {
         TableName: 'karma_points',
@@ -24,11 +26,43 @@ exports.handle = function(e, ctx, callback) {
         },
         ReturnValues:"UPDATED_NEW"
     }
-    docClient.update(params, function(err, data) {
-        if (err) {
-            callback(err, null)
-        } else {
-            callback(null, data)
+
+    docClient.get(
+        {
+            TableName: 'karma_points',
+            Key: {
+                "id": id
+            }
+        }, function(err, data) {
+            if (err) {
+                callback(err, null)
+            } else {
+                if(isEmpty(data)) {
+                    docClient.put(
+                        {
+                            TableName: 'karma_points',
+                            Item: {
+                                "id": id,
+                                "points": 1
+                            }
+                        }, function(err, data) {
+                            if (err) {
+                                callback(err, null)
+                            } else {
+                                callback(null, data)
+                            }
+                        }
+                    )
+                } else {
+                    docClient.update(params, function(err, data) {
+                        if (err) {
+                            callback(err, null)
+                        } else {
+                            callback(null, data)
+                        }
+                    })
+                }
+            }
         }
-    })
+    )
 }
